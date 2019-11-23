619e883e0c0aaa2b6beb3881b9773fed62b100d5
Jonas Neugebauer <jonas.neugebauer@uni-paderborn.de>
2019-11-23 16:13:38 +0100

    Erkennung ob Spiel vorbei verbessert
    Vorher wurde nicht immer erkannt, ob die Rover eine bestimmte Anzahl Züge lang
    keine Aktionen ausgeführt haben, da der Aktionszähler erhöht wurde, auch wenn die
    Aktion keinen Effekt hatte (z.B. `fahre()`, wenn keine Energie vorhanden ist).
    
    Das ist aus Sicht des Spiels sinnvoll (die Spieler müssen selber darauf achten, dass sie
    keine Züge verschwenden), aber für die Endbedingung dürfen nur Aktionen zählen,
    die auch einen Effekt haben. Daher wird nun separat pro Runde gezählt, wie viele Aktionen
    vom Rover ausgelöst wurden und wie viele einen Effekt hatten.

--- OOP/Greenfoot/06-RoverArena/Referee.java
+++ OOP/Greenfoot/06-RoverArena/Referee.java
@@ -17,6 +17,7 @@ public class Referee {
     public static final int RATE_CONVERT_MINERALS = 2;
 
     public static final int ENERGY_DRAIN = 10;
+    private static final byte TIMEOUT_AFTER_TURNS = 8;
 
 
     private static Referee ref;
@@ -84,8 +85,9 @@ public class Referee {
         } else {
             for( Rover r : rovers.keySet() ) {
                 RoverState state = getState(r);
-                if( !state.imobilized ) {
-                    updateState(r);
+                if( !state.immobilized ) {
+                    //updateState(r);
+                    checkState(r);
                     getState(r).addTurn();
                 }
             }
@@ -94,20 +96,23 @@ public class Referee {
 
     public void fahre( Rover pRover ) {
         if( checkState(pRover) ) {
+            RoverState state = getState(pRover);
             if( !pRover.huegelVorhanden("vorne") ) {
                 if( pRover.getEnergie() >= COSTS_MOVE ) {
                     pRover.move(1);
                     pRover.setEnergie(pRover.getEnergie()-COSTS_MOVE);
+                    state.didActionThisTurn = true;
                 }
             }
 
             updateState(pRover);
-            getState(pRover).addAction();
+            state.addAction();
         }
     }
 
     public void drehe( Rover pRover, String pRichtung ) {
         if( checkState(pRover) ) {
+            RoverState state = getState(pRover);
             if( pRover.getEnergie() >= COSTS_TURN ) {
                 if( pRichtung == "rechts" ) {
                     pRover.setRotation(pRover.getRotation() + 90);
@@ -115,15 +120,17 @@ public class Referee {
                     pRover.setRotation(pRover.getRotation() - 90);
                 }
                 pRover.setEnergie(pRover.getEnergie() - COSTS_TURN);
+                state.didActionThisTurn = true;
             }
 
             updateState(pRover);
-            getState(pRover).addAction();
+            state.addAction();
         }
     }
 
     public void analysiereGestein( Rover pRover ) {
         if( checkState(pRover) ) {
+            RoverState state = getState(pRover);
             if( pRover.getEnergie() >= COSTS_ANALYZE ) {
                 if( pRover.gesteinVorhanden() ) {
                     List<Gestein> stones = getWorld().getObjectsAt(
@@ -136,36 +143,38 @@ public class Referee {
                         getWorld().removeObject(stone);
 
                         pRover.setEnergie(pRover.getEnergie()-COSTS_ANALYZE);
+                        state.didActionThisTurn = true;
                     }
                 }
             }
 
             updateState(pRover);
-            getState(pRover).addAction();
+            state.addAction();
         }
     }
 
     public void setzeMarke( Rover pRover ) {
         if( checkState(pRover) ) {
+            RoverState state = getState(pRover);
             if( pRover.getEnergie() >= COSTS_SETMARK ) {
-                RoverState state = getState(pRover);
                 if( !pRover.markeVorhanden() && state.marks > 0 ) {
                     getWorld().addObject(new Marke(pRover), pRover.getX(), pRover.getY());
                     state.marks -= 1;
 
                     pRover.setEnergie(pRover.getEnergie()-COSTS_SETMARK);
+                    state.didActionThisTurn = true;
                 }
             }
             updateState(pRover);
-            getState(pRover).addAction();
+            state.addAction();
         }
     }
 
     public void entferneMarke( Rover pRover ) {
         if( checkState(pRover) ) {
+            RoverState state = getState(pRover);
             if( pRover.getEnergie() >= COSTS_DELMARK ) {
                 if( pRover.markeVorhanden() ) {
-                    RoverState state = getState(pRover);
                     List<Marke> marks = getWorld().getObjectsAt(
                         pRover.getX(), pRover.getY(), Marke.class);
                     if( marks.size() > 0 ) {
@@ -174,16 +183,18 @@ public class Referee {
                         state.marks += 1;
 
                         pRover.setEnergie(pRover.getEnergie()-COSTS_DELMARK);
+                        state.didActionThisTurn = true;
                     }
                 }
             }
             updateState(pRover);
-            getState(pRover).addAction();
+            state.addAction();
         }
     }
 
     public void konvertiereWasser( Rover pRover, int pMenge, String pProdukt ) {
         if( checkState(pRover) ) {
+            RoverState state = getState(pRover);
             if( pProdukt.equals("energie") ) {
                 if( pRover.getWasser() < pMenge ) {
                     pMenge = pRover.getWasser();
@@ -200,15 +211,17 @@ public class Referee {
                 pRover.setMineralien(pRover.getMineralien() + (pMenge*RATE_CONVERT_MINERALS));
                 pRover.setEnergie(pRover.getEnergie()-(pMenge*COSTS_CONVERT_MINERALS));
                 pRover.setWasser(pRover.getWasser()-pMenge);
+                state.didActionThisTurn = true;
             }
 
             updateState(pRover);
-            getState(pRover).addAction();
+            state.addAction();
         }
     }
 
     public void energieAbziehen( Rover pRover ) {
         if( checkState(pRover) ) {
+            RoverState state = getState(pRover);
             if( pRover.roverVorhanden("vorne") ) {
                 int[] vorne = getFieldAt(pRover, "vorne");
                 Rover otherRover = getOneObjectAt(vorne[0], vorne[1], Rover.class);
@@ -219,7 +232,8 @@ public class Referee {
 
                     updateState(otherRover);
                     updateState(pRover);
-                    getState(pRover).addAction();
+                    state.didActionThisTurn = true;
+                    state.addAction();
                 }
             }
         }
@@ -234,35 +248,58 @@ public class Referee {
         }
     }
 
-    private int[] getFieldAt( Rover pRover, String richtung ) {
+    private <T> T getOneObjectAtOffset( Rover pRover, int xOffset, int yOffset, Class<T> clazz ) {
+        List<T> objects = getWorld().getObjectsAt(pRover.getX()+xOffset, pRover.getY()+yOffset, clazz);
+        if( objects.size() > 0 ) {
+            return objects.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    private <T> T getOneObjectAtDirection( Rover pRover, String direction, Class<T> clazz ) {
+        int[] coords = getFieldAt(pRover, direction);
+        return getOneObjectAt(coords[0], coords[1], clazz);
+    }
+
+    private int[] getFieldAt( Rover pRover, String direction ) {
+        int[] offset = getOffsetForDirection(pRover, direction);
+        return new int[]{pRover.getX()+offset[0],pRover.getY()+offset[1]};
+    }
+
+    private int[] getOffsetForDirection( Rover pRover, String direction ) {
         int rot = pRover.getRotation(), x = pRover.getX(), y = pRover.getY();
 
-        if( richtung == "vorne" && rot == 0 || richtung == "rechts" && rot == 270
-            || richtung == "links" && rot == 90 ) {
-            return new int[]{x+1,y};
+        if( direction == "vorne" && rot == 0 || direction == "rechts" && rot == 270
+            || direction == "links" && rot == 90 ) {
+            return new int[]{1,0};
         }
 
-        if( richtung == "vorne" && rot == 180 || richtung == "rechts" && rot == 90
-            || richtung == "links" && rot == 270 ) {
-            return new int[]{x-1,y};
+        if( direction == "vorne" && rot == 180 || direction == "rechts" && rot == 90
+            || direction == "links" && rot == 270 ) {
+            return new int[]{-1,0};
         }
 
-        if( richtung == "vorne" && rot == 90 || richtung == "rechts" && rot == 0
-            || richtung == "links" && rot == 180 ) {
-            return new int[]{x,y+1};
+        if( direction == "vorne" && rot == 90 || direction == "rechts" && rot == 0
+            || direction == "links" && rot == 180 ) {
+            return new int[]{0,1};
         }
 
-        if( richtung == "vorne" && rot == 270 || richtung == "rechts" && rot == 180
-            || richtung == "links" && rot == 0 ) {
-            return new int[]{x,y-1};
+        if( direction == "vorne" && rot == 270 || direction == "rechts" && rot == 180
+            || direction == "links" && rot == 0 ) {
+            return new int[]{0,-1};
         }
 
-        return new int[]{x+1,y};
+        return new int[]{1,0};
     }
 
     private void updateState( Rover pRover ) {
         RoverState state = getState(pRover);
         if( state != null ) {
+            if( pRover.getWorld() == null ) {
+                getWorld().addObject(pRover, state.xPos, state.yPos);
+            }
+
             state.xPos = pRover.getX();
             state.yPos = pRover.getY();
             state.rotation = pRover.getRotation();
@@ -298,7 +335,7 @@ public class Referee {
                 System.err.println(pRover.getName()+" wurde bestraft für den Versuch, seine Resourcen unerlaubt zu ändern.");
             }
 
-            if( !state.imobilized && state.actionsThisTurn < 2 ) {
+            if( !state.immobilized && state.actionsThisTurn < 2 ) {
                 return true;
             }
         } else {
@@ -322,10 +359,16 @@ public class Referee {
             // checkState(r);
             RoverState state = getState(r);
 
-            if( r.getEnergie() < COSTS_MOVE && r.getWasser() <= 0 ) {
-                state.imobilized = true;
+            if( state.immobilized ) {
+                continue;
             }
-            if( !state.imobilized ) {
+
+            if( (r.getEnergie() < COSTS_MOVE && r.getWasser() <= 0)
+                    || state.turnsWithoutActions >= TIMEOUT_AFTER_TURNS ) {
+                state.immobilized = true;
+                System.out.printf("%s ist ausgeschieden. (%d Mineralien gesammelt.)\n", r.getName(), r.getMineralien());
+            }
+            if( !state.immobilized ) {
                 allImmobile = false;
             }
         }
@@ -365,14 +408,20 @@ public class Referee {
         public long turn = 0, actionsTotal = 0;
 
         // 0, 1 or 2
-        public byte actionsThisTurn = 0;
+        public byte actionsThisTurn = 0, turnsWithoutActions = 0;
 
         public int penalties = 0;
 
-        public boolean imobilized = false;
+        public boolean immobilized = false, didActionThisTurn = false;
 
         public void addTurn() {
             turn += 1;
+            if( !didActionThisTurn ) {
+                turnsWithoutActions += 1;
+            } else {
+                turnsWithoutActions = 0;
+                didActionThisTurn = false;
+            }
             actionsThisTurn = 0;
         }
 
@@ -382,7 +431,7 @@ public class Referee {
         }
     }
 
-    private class ActorDelegate extends Actor {
+    class ActorDelegate extends Actor {
         Referee ref;
         public ActorDelegate( Referee ref ) {
             this.ref = ref;
@@ -393,6 +442,7 @@ public class Referee {
         }
 
         public void addedToWorld(World world) {
+            setImage("images/boden.png");
             this.ref.addedToWorld(world);
         }
     }
