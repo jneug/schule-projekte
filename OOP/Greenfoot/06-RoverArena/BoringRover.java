edb84e4ca5fb7dad7db8ee8c26483ed69d96a2c1
Jonas Neugebauer <jonas.neugebauer@uni-paderborn.de>
2019-12-10 20:08:24 +0100

    Ausgabe des aktuellen Verhaltens ergänzt

--- OOP/Greenfoot/06-RoverArena/BoringRover.java
+++ OOP/Greenfoot/06-RoverArena/BoringRover.java
@@ -6,24 +6,47 @@ public class BoringRover extends Rover {
 
     public void act() {
         if( getEnergie() < 100 /*&& getWasser() > 0*/ ) {
+            ausgabe("Konvertiere 500 Wasser in Energie");
             konvertiereWasser(500, "energie");
         } else if( getWasser() >= 600 ) {
-            konvertiereWasser(getEnergie()-200, "mineralien");
+            ausgabe("Konvertiere "+(getWasser() - 200)+" Wasser in Mineralien");
+            konvertiereWasser(getWasser() - 200, "mineralien");
+        } else if( roverVorhanden("vorne") ) {
+            ausgabe("Entziehe anderem Rover Energie");
+            entzieheEnergie();
+            entzieheEnergie();
         } else if( gesteinVorhanden() ) {
+            ausgabe("Analysiere Gestein");
             analysiereGestein();
         } else if( huegelVorhanden("vorne") ) {
             if( !huegelVorhanden("rechts") ) {
+                ausgabe("Huegel vor mir, drehe rechts.");
                 drehe("rechts");
                 fahre();
             } else if( !huegelVorhanden("links") ) {
+                ausgabe("Huegel vor mir, drehe links.");
                 drehe("links");
                 fahre();
             } else {
+                ausgabe("Huegel überall, drehe um.");
                 drehe("links");
                 drehe("links");
             }
         } else {
+            ausgabe("Freie fahrt!");
             fahre();
+        }
+    }
+
+    /*
+        Hilfsmethode, um einen Text auszugeben.
+     */
+    private void ausgabe( String pText ) {
+        // Diesen Wert auf "true" setzen, um Ausgaben zu erhalten!
+        boolean ausgabeEin = false;
+
+        if( ausgabeEin ) {
+            System.out.println("<"+name+"> sagt: "+pText);
         }
     }
 
