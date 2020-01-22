387d23304e3b4fd1bd347c30dfd335d535e60aab
Jonas Neugebauer <jonas.neugebauer@uni-paderborn.de>
2020-01-22 17:00:56 +0100

    Projektgerüst zur Implementierung von Suchalgorithmen

---
+++ Algorithmen/Suchen und Sortieren/BlueJ/Sortiermaschine/Suchmaschine_2/Suchmaschine.java
@@ -0,0 +1,24 @@
+
+public class Suchmaschine {
+
+    /**
+     * Sucht das erste vorkommen von <code>suchzahl</code> in
+     * <code>sucharray</code>. Ist die Zahl vorhanden, wird <code>true</code>
+     * zurück gegeben, sonst <code>false</code>.
+     *
+     * Die Methode nutzt zur Suche die binäre Suche. Daher muss
+     * <code>sucharray</code> aufsteigend vorsortiert sein.
+     */
+    public boolean binaereSucheIterativ( int suchzahl, int[] sucharray ) {
+        return false;
+    }
+
+    public boolean binaereSucheRekursiv( int suchzahl, int[] sucharray ) {
+        return false;
+    }
+
+    public boolean binaereSucheRekursiv( int suchzahl, int[] sucharray, int von, int bis ) {
+        return false;
+    }
+
+}
