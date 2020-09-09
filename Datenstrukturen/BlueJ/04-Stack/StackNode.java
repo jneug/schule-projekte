20def281e6347f1609e0bad23b118d5594ff559a
Jonas Neugebauer <jonas@neugebauer.cc>
2020-09-09 22:31:04 +0200

    [Vorlagen] Beispielschaltungen für den Einsteig in Digital

---
+++ Datenstrukturen/BlueJ/04-Stack/StackNode.java
@@ -0,0 +1,47 @@
+
+public class StackNode<ContentType> {
+
+    private ContentType contentObject = null;
+
+    private StackNode nextNode = null;
+
+    /**
+     * Ein neues Objekt vom Typ Node<ContentType> wird erschaffen.
+     * Der Inhalt wird per Parameter gesetzt. Der Verweis ist leer.
+     *
+     * @param pContentObject das Inhaltselement des Knotens vom Typ ContentType
+     */
+    public StackNode( ContentType pContentObject) {
+        contentObject = pContentObject;
+        nextNode = null;
+    }
+
+    /**
+     * Der Verweis wird auf das Objekt, das als Parameter uebergeben wird,
+     * gesetzt.
+     *
+     * @param pNext der Nachfolger des Knotens
+     */
+    public void setNext( StackNode pNext) {
+        nextNode = pNext;
+    }
+
+    /**
+     * Liefert das naechste Element des aktuellen Knotens.
+     *
+     * @return das Objekt vom Typ QueueNode, auf das der aktuelle Verweis zeigt
+     */
+    public StackNode getNext() {
+        return nextNode;
+    }
+
+    /**
+     * Liefert das Inhaltsobjekt des Knotens vom Typ ContentType.
+     *
+     * @return das Inhaltsobjekt des Knotens
+     */
+    public ContentType getContent() {
+        return contentObject;
+    }
+
+}
