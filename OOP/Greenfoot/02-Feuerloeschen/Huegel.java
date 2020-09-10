5b1d9489a85b95b9b0fc8e043ebd7b3ff9e7e1c7
Jonas Neugebauer <jonas@neugebauer.cc>
2020-09-10 07:24:02 +0200

    Neues Szenario "Feuerlöschen" als Alternative zum Mars-Rover
    Alle assets und Code unter CC-Lizenzen

---
+++ OOP/Greenfoot/02-Feuerloeschen/Huegel.java
@@ -0,0 +1,37 @@
+import greenfoot.*;
+
+/**
+ * Ein Hügel in der Mars-Welt. Ein Hügel ist ein Hindernis für dne Rover, das
+ * nicht befahren werden kann.
+ */
+public class Huegel extends Actor {
+
+    private int steigung;
+
+    public Huegel() {
+        this(-1);
+    }
+
+    public Huegel( int pSteigung ) {
+        if( pSteigung < 31 || pSteigung > 60 ) {
+            steigung = Greenfoot.getRandomNumber(30) + 31;
+        } else {
+            steigung = pSteigung;
+        }
+        int num = Greenfoot.getRandomNumber(47)+1;
+        setImage("images/trees/tree"+num+".png");
+    }
+
+    /**
+     * Act-Methode des Hügels.
+     */
+    public void act() {
+        // Der Hügel hat keine Funktion.
+    }
+
+    public int getSteigung() {
+        return steigung;
+    }
+
+}
+
---
+++ OOP/Greenfoot/02-Feuerloeschen/Marke.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Eine Markierung, die ein Rover ablegen oder einsammeln kann.
+ */
+public class Marke extends Actor {
+
+    public Marke() {
+        setImage("images/flamme.png");
+    }
+
+    /**
+     * Act-Methode der Marke.
+     */
+    public void act() {
+        // Die Marke hat keine Funktion.
+    }
+
+}
