34b4e124b50a906033c9f5055542aba72d53749b
J. Neugebauer <ngb@helmholtz-bi.de>
2021-02-26 11:12:12 +0100

    Gegenstände und Gegener eingefügt

--- OOP/BlueJ/40-Zulda/Gegner.java
+++ OOP/BlueJ/40-Zulda/Gegner.java
@@ -1,8 +1,12 @@
 import ea.*;
 
-import java.util.Random;
-
-public abstract class Enemy extends Bild {
+/**
+ * Abstrakte Basiklasse für Gegner.
+ *
+ * Gegener kennen immer die Karte, auf der sie sich befinden. Sie können ihre Karte
+ * nicht verlassen.
+ */
+public abstract class Gegner extends Bild {
 
     protected Karte karte;
 
@@ -12,15 +16,46 @@ public abstract class Enemy extends Bild {
 
     private int defense;
 
-    public Enemy( Karte pKarte, String pBild ) {
+    public Gegner(int pHitpoints, int pAttack, int pDefense, Karte pKarte, String pBild ) {
         super(0, 0, pBild);
 
         karte = pKarte;
+
+        hitpoints = pHitpoints;
+        attack    = pAttack;
+        defense   = pDefense;
     }
 
     public Karte getKarte() {
         return karte;
     }
 
+    public void addHitpoints( int pHp ) {
+        hitpoints += pHp;
+    }
+
+    public int getHitpoints() {
+        return hitpoints;
+    }
+
+    public void setHitpoints(int hitpoints) {
+        this.hitpoints = hitpoints;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
 
 }
--- OOP/BlueJ/40-Zulda/Karte_0.java
+++ OOP/BlueJ/40-Zulda/Karte_0.java
@@ -3,25 +3,85 @@
  */
 public class Karte_0 extends Karte {
 
+    private Ork feind;
+
+    private TrankAngriff trank;
+
     public Karte_0( int x, int y ) {
         super(x,y);
 
         for( int i = 0; i < felder.length; i++ ) {
             if( i < 9 || i > 10 ) {
-                felder[i][0] = new Feld(i * 48, 0, "stein");
-                add(felder[i][0]);
-                felder[i][14] = new Feld(i * 48, 14 * 48, "stein");
-                add(felder[i][14]);
+                addFeld(i, 0, "stein");
             }
+            addFeld(i, 14, "stein");
         }
         for (int j = 0; j < felder[0].length; j++) {
             if( j != 7 ){
-                felder[0][j] = new Feld(0, j * 48, "stein");
-                add(felder[0][j]);
-                felder[19][j] = new Feld(19 * 48, j * 48, "stein");
-                add(felder[19][j]);
+                addFeld(0, j, "stein");
+                addFeld(19, j, "stein");
             }
         }
+
+        int centerX = 4, centerY = 5;
+        addFeld(centerX, centerY-1, "wasser");
+        addFeld(centerX-1, centerY, "wasser");
+        addFeld(centerX, centerY, "wasser");
+        addFeld(centerX+1, centerY, "wasser");
+        addFeld(centerX, centerY+1, "wasser");
+
+        addFeld(centerX, centerY-2, "sand");
+        addFeld(centerX-2, centerY, "sand");
+        addFeld(centerX, centerY+2, "sand");
+        addFeld(centerX+2, centerY, "sand");
+        addFeld(centerX-1, centerY-1, "sand");
+        addFeld(centerX+1, centerY-1, "sand");
+        addFeld(centerX+1, centerY+1, "sand");
+        addFeld(centerX-1, centerY+1, "sand");
+
+        centerX = 11;
+        centerY = 5;
+        for( int i = 0; i < 6; i++ ) {
+            for (int j = 0; j < 6; j++) {
+                if( i == 0 || j == 0 || i == 5 || j == 5 ) {
+                    addFeld(centerX+i, centerY+j, "sand");
+                } else {
+                    addFeld(centerX+i, centerY+j, "stein");
+                }
+            }
+        }
+
+        // Einen Ork als Gegner einfügen
+        feind = new Ork(this);
+        verschiebeZuFeldAnIndex(feind,2, 2);
+        add(feind);
+
+        // Einen Trank als Gegenstand einfügen
+        addFeld(18, 13, "sand");
+        trank = new TrankAngriff(this);
+        verschiebeZuFeldAnIndex(trank, 18,13);
+        add(trank);
+    }
+
+    /**
+     * Hilfsmethode, um einfach ein Feld an einem Index i,j in die Karte einzufügen.
+     * @param i i-Index des Feldes
+     * @param j j-Index des Feldes
+     * @param typ Typ des Untergrundes (bestimmt auch die Passierbarkeit)
+     */
+    private void addFeld(int i, int j, String typ ) {
+        felder[i][j] = new Feld(i*48, j * 48, typ);
+        add(felder[i][j]);
+    }
+
+    @Override
+    public void karteAnzeigen() {
+        feind.start();
+    }
+
+    @Override
+    public void karteVerstecken() {
+        feind.stopp();
     }
 
 }
